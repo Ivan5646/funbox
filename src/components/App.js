2f844d278a5925861227057dde6813aabada1963
Ivan5646 <Mike000027@yandex.ru>
2019-03-11 00:18:29 +0700

    initial

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Cats from './Cats'
+
+const App = () => (
+    <div>
+        <Cats />
+    </div>
+)
+
+export default App;
---
+++ src/components/Cats.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class Cats extends React.Component {
+  componentDidMount() {
+  }
+
+  render() {
+    return (
+      <h3>Cats</h3>
+    )
+  }
+}
+
+export default Cats;
