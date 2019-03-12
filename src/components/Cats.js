0cec04594036303eeca3df8e81658f18c2f38f1a
Ivan5646 <Mike000027@yandex.ru>
2019-03-12 23:54:31 +0700

    cards started

--- src/components/Cats.js
+++ src/components/Cats.js
@@ -10,16 +10,34 @@ class Cats extends React.Component {
     renderCards () {
         console.log(food);
         return food.map(item => (
-            <div key={item.id}>{item.title}</div>
+            <div className="cats__card-wrapper">
+                <div key={item.id} className="cats__card">
+                    <div className="cats__card-top">
+                        <div className="cats__card-header">Сказочное заморское яство</div>
+                        <h1>Нямушка</h1>
+                        <h2>{item.title}</h2>
+                        <div className="cats__card-details">
+                            <div>{item.amount} порций</div>
+                            <div>{item.bonus} мышь в подарок</div>
+                        </div>
+                    </div>
+                    <div className="cats__card-bottom">
+                        <div className="cats__cat"></div>
+                        <div>{item.weight}</div>
+                    </div>
+                </div>
+                <div className="cats__description">{item.description}</div>
+            </div>
         ))
     }
 
     render() {
-        // console.log(food);
         return (
             <div className="cats">
-                <h3>Cats</h3>
-                {this.renderCards()}
+                <h3 className="cats__header">Ты сегодня покормил кота?</h3>
+                <div className="cats__wrapper">
+                    {this.renderCards()}
+                </div>
             </div>
 
         )
--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const food = [
-    {_id: "1", title: "с фуагра", amount: 10, weight: 0.5, inStock: true},
-    {_id: "2", title: "с рыбой", amount: 40,  weight: 5, inStock: true},
-    {_id: "3", title: "с курой", amount: 100,  weight: 5, inStock: true}
+    {_id: "1", title: "с фуа-гра", amount: 10, weight: 0.5, bonus: 1, inStock: true, description: "Печень утки разварная с артишоками."},
+    {_id: "2", title: "с рыбой", amount: 40,  weight: 5, bonus: 2, inStock: true, description: "Головы щучьи с чесноком да свежайшая сёмгушка."},
+    {_id: "3", title: "с курой", amount: 100,  weight: 5, bonus: 5, inStock: true, description: "Филе из цыплят с трюфелями в бульоне."}
 ];
